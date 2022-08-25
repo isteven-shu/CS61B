7587c467e4b725d8b96f0ed7e5aa8e38748f9aca
shuyuan <noahdream@163.com>
2022-08-25 23:31:59 +0800

    lab6

---
+++ lab6/UnionFind.java
@@ -0,0 +1,91 @@
+public class UnionFind {
+
+    // TODO - Add instance variables?
+    private int[] parent;
+
+    /* Creates a UnionFind data structure holding n vertices. Initially, all
+       vertices are in disjoint sets. */
+    public UnionFind(int n) {
+        // TODO
+        parent = new int[n];
+        for (int i = 0; i < n; ++i) {
+            parent[i] = -1;
+        }
+    }
+
+    /* Throws an exception if v1 is not a valid index. */
+    private void validate(int vertex) {
+        // TODO
+        if (vertex < 0 || vertex > parent.length) {
+            throw new IllegalArgumentException("invalid vertex");
+        }
+    }
+
+    /* Returns the size of the set v1 belongs to. */
+    public int sizeOf(int v1) {
+        // TODO
+        validate(v1);
+        if (parent[v1] < 0) {
+            return -parent[v1];
+        }
+        return sizeOf(parent[v1]);
+    }
+
+    /* Returns the parent of v1. If v1 is the root of a tree, returns the
+       negative size of the tree for which v1 is the root. */
+    public int parent(int v1) {
+        // TODO
+        return parent[v1];
+    }
+
+    /* Returns true if nodes v1 and v2 are connected. */
+    public boolean connected(int v1, int v2) {
+        // TODO
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
+    }
+
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
+       elements, and a union-by-size heuristic is used. If the sizes of the sets
+       are equal, tie break by connecting v1's root to v2's root. Unioning a 
+       vertex with itself or vertices that are already connected should not 
+       change the sets but may alter the internal structure of the data. */
+    public void union(int v1, int v2) {
+        // TODO
+        validate(v1);
+        validate(v2);
+
+        int r1 = find(v1);
+        int r2 = find(v2);
+
+        if (r1 == r2) {
+            return;
+        }
+        if (-parent[r1] <= -parent[r2]) {
+            parent[r2] += parent[r1];   // 这个的顺序不能放到下一行后面
+            parent[r1] = r2;
+        } else {
+            parent[r1] += parent[r2];
+            parent[r2] = r1;
+        }
+    }
+
+    /* Returns the root of the set V belongs to. Path-compression is employed
+       allowing for fast search-time. */
+    public int find(int vertex) {
+        // TODO
+        validate(vertex);
+        return findHelper(vertex);
+    }
+
+    private int findHelper(int vertex) {
+        if (parent[vertex] < 0) {
+            return vertex;
+        }
+
+        int result = findHelper(parent[vertex]);
+        parent[vertex] = result;
+        return result;
+    }
+}
