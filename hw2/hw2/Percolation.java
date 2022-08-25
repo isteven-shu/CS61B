90806795a1eab2e28e64d3c3bd0d698a5a8fb218
shuyuan <noahdream@163.com>
2022-08-26 00:29:42 +0800

    Using 2 UF to fix the bug for percolate()

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,16 +4,17 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     /** N in N * N grids */
-    int N;
+    private int N;
 
     /** Number of open boxes */
-    int numOfOpen;
+    private int numOfOpen;
 
     /** true if open, false if not */
-    boolean[][] isOpen;
+    private boolean[][] isOpen;
 
     /** Union find */
-    WeightedQuickUnionUF grid;
+    private WeightedQuickUnionUF gridForPercolate;
+    private WeightedQuickUnionUF gridForIsFull;
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
@@ -27,10 +28,9 @@ public class Percolation {
 
         /** totally N^2 + 2 length for UF, 1--N^2 represents normal boxes
          *  0 represents the top virtual node, N * N + 1 represents the bottom virtual node
-         *  top virtual node is connected tp every open node in the top row
-         *  bottom virtual node is connected to every full node in the bottom row
-         *  */
-        grid = new WeightedQuickUnionUF(N * N + 2);
+         */
+        gridForIsFull = new WeightedQuickUnionUF(N * N + 1);
+        gridForPercolate = new WeightedQuickUnionUF(N * N + 2);
     }
 
     private int convertTo1D(int row, int col) {
@@ -52,32 +52,35 @@ public class Percolation {
         isOpen[row][col] = true;
 
         if (row == 0) {
-            grid.union(idx, 0);
+            gridForIsFull.union(idx, 0);
+            gridForPercolate.union(idx, 0);
+        } else if (row == N - 1) {
+            gridForPercolate.union(idx, N * N + 1);
         }
 
         if (row - 1 >= 0) {
             if (isOpen(row - 1, col)) {
-                grid.union(idx, convertTo1D(row - 1, col));
+                gridForIsFull.union(idx, convertTo1D(row - 1, col));
+                gridForPercolate.union(idx, convertTo1D(row - 1, col));
             }
         }
         if (row + 1 < N) {
             if (isOpen(row + 1, col)) {
-                grid.union(idx, convertTo1D(row + 1, col));
+                gridForIsFull.union(idx, convertTo1D(row + 1, col));
+                gridForPercolate.union(idx, convertTo1D(row + 1, col));
             }
         }
         if (col - 1 >= 0) {
             if (isOpen(row, col - 1)) {
-                grid.union(idx, convertTo1D(row, col - 1));
+                gridForIsFull.union(idx, convertTo1D(row, col - 1));
+                gridForPercolate.union(idx, convertTo1D(row, col - 1));
             }
         }
         if (col + 1 < N) {
             if (isOpen(row, col + 1)) {
-                grid.union(idx, convertTo1D(row, col + 1));
+                gridForIsFull.union(idx, convertTo1D(row, col + 1));
+                gridForPercolate.union(idx, convertTo1D(row, col + 1));
             }
-        }
-
-        if (row == N - 1 && isFull(row, col)) {
-            grid.union(idx, N * N + 1);
         }
     }
 
@@ -91,7 +94,7 @@ public class Percolation {
     public boolean isFull(int row, int col) {
         validate(row, col);
         int idx = convertTo1D(row, col);
-        return grid.connected(idx, 0);
+        return gridForIsFull.connected(idx, 0);
     }
 
 
@@ -103,11 +106,12 @@ public class Percolation {
     // does the system percolate?
     // We say the system percolates if there is a full site in the bottom row.
     public boolean percolates() {
-        return grid.connected(0, N * N + 1);
+        return gridForPercolate.connected(0, N * N + 1);
     }
 
     // use for unit testing (not required, but keep this here for the autograder)
     public static void main(String[] args) {
+        Percolation p = new Percolation(6);
 
     }
 }
