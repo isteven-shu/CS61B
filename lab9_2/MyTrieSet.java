c11a9167bde52c4097165430e37bb625df68e914
shuyuan <noahdream@163.com>
2022-09-04 01:16:18 +0800

    lalb9_2019 Trie

---
+++ lab9_2/MyTrieSet.java
@@ -0,0 +1,131 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
+public class MyTrieSet implements TrieSet61B{
+    private class Node {
+        boolean isKey;
+        HashMap<Character, Node> links;
+
+        Node(boolean isKey) {
+            this.isKey = isKey;
+            links = new HashMap<>();
+        }
+    }
+
+    Node root;
+    int size;
+
+    MyTrieSet() {
+        root = new Node(false);
+        size = 0;
+    }
+
+    @Override
+    public void clear() {
+        root = null;
+        size = 0;
+    }
+
+    /**
+     @Override
+     public void add(String key) {
+         if (key == null || key.length() < 1) {
+            return;
+         }
+         Node curr = root;
+         for (int i = 0, n = key.length(); i < n; i++) {
+             char c = key.charAt(i);
+             if (!curr.map.containsKey(c)) {
+                curr.map.put(c, new Node(c, false));
+             }
+            curr = curr.map.get(c);
+         }
+         curr.isKey = true;
+     }
+     */
+
+    @Override
+    public void add(String key) {
+        root = add(root, key, 0);
+    }
+
+    private Node add(Node root, String key, int d) {
+        if (root == null) {
+            root = new Node(false);
+        }
+        if (d == key.length()) {
+            root.isKey = true;
+            return root;
+        }
+
+        char c = key.charAt(d);
+        root.links.put(c, add(root.links.get(c), key, d + 1));
+        return root;
+    }
+
+    @Override
+    public boolean contains(String key) {
+        return contains(root, key, 0);
+    }
+
+    private boolean contains(Node root, String key, int d) {
+        if (root == null) {
+            return false;
+        }
+        if (d == key.length()) {
+            return root.isKey;
+        }
+
+        char c = key.charAt(d);
+        return contains(root.links.get(c), key, d + 1);
+    }
+
+    private Node get(Node root, String key, int d) {
+        if (root == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            return root;
+        }
+
+        char c = key.charAt(d);
+        return get(root.links.get(c), key, d + 1);
+    }
+
+    @Override
+    public List<String> keysWithPrefix(String prefix) {
+        ArrayList<String> myArrayList = new ArrayList<>();
+        collect(get(root, prefix, 0), prefix, myArrayList);
+        return myArrayList;
+    }
+
+    /** Collect all the keys starts from root into the list */
+    private void collect(Node root, String curKey, ArrayList<String> myList) {
+        if (root == null) {
+            return;
+        }
+        if (root.isKey == true) {
+            myList.add(curKey);
+        }
+
+        for (char c : root.links.keySet()) {
+            collect(root.links.get(c), curKey + c, myList);
+        }
+    }
+
+    @Override
+    public String longestPrefixOf(String s) {
+        if (s == null)    return null;
+        String answer = "";
+        longestPrefixOf(root, s, answer, "");
+        return answer;
+    }
+
+    private void longestPrefixOf(Node root, String s, String curLongestKey, String curString) {
+        if (root == null)   return;
+        if (root.isKey)     curLongestKey = curString;
+        char c = s.charAt(curString.length());
+        longestPrefixOf(root.links.get(c), s, curLongestKey, curString + c);
+    }
+}
